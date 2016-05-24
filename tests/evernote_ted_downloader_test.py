6efe377828318a7df72f91f8d8079937ecf44e5a
Christian Dähn <christian.daehn@gmail.com>
2016-05-24 22:03:45 +0200

    readd metalink

--- tests/evernote_ted_downloader_test.py
+++ tests/evernote_ted_downloader_test.py
@@ -36,23 +36,13 @@ class EvernoteTest(unittest.TestCase):
 		for ted_note in evernote_parser.get_ted_notes():
 			self.assertEqual('http://download.ted.com/talks/JoeGebbia_2016-480p-en.mp4', metalink_parser.get_metalink(ted_note).url)
 	def test_download_all_ted_notes_from_metalink(self):
-		
-
-
-
-
 		evernote_parser = EvernoteTedParser('S=s1:U=9288d:E=15c3a9bc252:C=154e2ea94e8:P=1cd:A=en-devtoken:V=2:H=c0cba7347df5a0c22cce3fd7d33771de', 
 											'daehn')
-
 		ted_downloader = TedDownloader('http://metated.petarmaric.com/metalinks/TED-talks-in-high-quality.en.metalink', 'tests')
 
 		for ted_note in evernote_parser.get_ted_notes():
 			file = ted_downloader.download(ted_note)
 			self.assertTrue(os.path.exists(file))
-
-
-
-
 
 if __name__ == '__main__':
 	import logging
